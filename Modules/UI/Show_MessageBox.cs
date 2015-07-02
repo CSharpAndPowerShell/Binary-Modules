68ed07238c6d7ca7e436f0384daa3577aedcba59
Cristopher Robles Ríos <crisrc012@hotmail.com>
2015-07-02 17:04:40 -0600

    Nuevo
    Módulo nuevo

---
+++ Modules/UI/Show_MessageBox.cs
@@ -0,0 +1,42 @@
+﻿using System.Management.Automation; //Windows PowerShell NameSpace
+
+namespace UI
+{
+    //Define el nombre del Cmdlet
+    [Cmdlet(VerbsCommon.Show, "MessageBox")]
+    public class Show_MessageBox : Cmdlet
+    {
+        #region Objects
+        private UICommon SMB;
+        #endregion
+        #region Parameters
+        [Parameter(Position = 0, Mandatory = false, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Nombre del nuevo usuario.")]
+        public string Message { get; set; }
+        [Parameter(Position = 1, Mandatory = false, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Contraseña para el nuevo usuario.")]
+        public string Title { get; set; }
+        [Parameter(Position = 2, Mandatory = false, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Letra en la que se montará el 'HomeDirectory'.")]
+        [ValidateSet("Asterisk", "Error", "Exclamation", "Hand", "Information", "None", "Question", "Stop", "Warning")]
+        public string Icon { get; set; }
+        [Parameter(Position = 3, Mandatory = false, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Letra en la que se montará el 'HomeDirectory'.")]
+        [ValidateSet("AbortRetryIgnore", "OK", "OKCancel", "RetryCancel", "YesNo", "YesNoCancel")]
+        public string Buttons { get; set; }
+        #endregion
+        #region Methods
+        protected override void BeginProcessing()
+        {
+            SMB = new UICommon();
+        }
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                SMB.ShowMessageBox(Message, Title, Buttons, Icon);
+            }
+            catch (PSInvalidOperationException e)
+            {
+                WriteError(e.ErrorRecord);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Modules/UI/UICommon.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows.Forms;
+
+namespace UI
+{
+    public class UICommon
+    {
+        public void ShowMessageBox(string Message, string Title, string Buttons, string Icon)
+        {
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            switch (Buttons) {
+                case "AbortRetryIgnore":
+                    buttons = MessageBoxButtons.AbortRetryIgnore;
+                    break;
+                case "OK":
+                    buttons = MessageBoxButtons.OK;
+                    break;
+                case "OKCancel":
+                    buttons = MessageBoxButtons.OKCancel;
+                    break;
+                case "RetryCancel":
+                    buttons = MessageBoxButtons.RetryCancel;
+                    break;
+                case "YesNo":
+                    buttons = MessageBoxButtons.YesNo;
+                    break;
+                case "YesNoCancel":
+                    buttons = MessageBoxButtons.YesNoCancel;
+                    break;
+                default:
+                    buttons = MessageBoxButtons.OK;
+                    break;
+            }
+            MessageBoxIcon icon = MessageBoxIcon.None;
+            switch (Icon)
+            {
+                case "Asterisk":
+                    icon = MessageBoxIcon.Asterisk;
+                    break;
+                case "Error":
+                    icon = MessageBoxIcon.Error;
+                    break;
+                case "Exclamation":
+                    icon = MessageBoxIcon.Exclamation;
+                    break;
+                case "Hand":
+                    icon = MessageBoxIcon.Hand;
+                    break;
+                case "Information":
+                    icon = MessageBoxIcon.Information;
+                    break;
+                case "None":
+                    icon = MessageBoxIcon.None;
+                    break;
+                case "Question":
+                    icon = MessageBoxIcon.Question;
+                    break;
+                case "Stop":
+                    icon = MessageBoxIcon.Stop;
+                    break;
+                case "Warning":
+                    icon = MessageBoxIcon.Warning;
+                    break;
+                default:
+                    icon = MessageBoxIcon.None;
+                    break;
+            }
+            MessageBox.Show(Message, Title, buttons, icon);
+        }
+    }
+}
