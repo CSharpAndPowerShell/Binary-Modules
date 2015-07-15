fc1ef0f2d318e37e7b9c748b0dec2e0c888368a8
Cristopher Robles Ríos <crisrc012@hotmail.com>
2015-07-15 13:10:01 -0600

    Módulo nuevo

---
+++ Modules/Drive/DriveCommon.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Drive
+{
+    public class DriveCommon
+    {
+        private Shell32.Shell shell = new Shell32.Shell();
+        public void RenameDrive(char letter, string name) {
+            //Renombra unidades locales y de red
+            ((Shell32.Folder2)shell.NameSpace(letter + ":")).Self.Name = name;
+        }
+    }
+}
---
+++ Modules/Drive/Rename_Drive.cs
@@ -0,0 +1,37 @@
+﻿using System.Management.Automation; //Windows PowerShell NameSpace
+
+namespace Drive
+{
+    [Cmdlet(VerbsCommon.Rename, "Drive")]
+    public class Rename_Drive : Cmdlet
+    {
+        #region Objects
+        private DriveCommon RD;
+        #endregion
+        #region Parameters
+        [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Letra de la unidad.")]
+        [ValidateNotNullOrEmpty]
+        public char Letter { get; set; }
+        [Parameter(Position = 1, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Nombre de la unidad.")]
+        [ValidateNotNullOrEmpty]
+        public string Name { get; set; }
+        #endregion
+        #region Methods
+        protected override void BeginProcessing()
+        {
+            RD = new DriveCommon();
+        }
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                RD.RenameDrive(Letter, Name);
+            }
+            catch (PSInvalidOperationException e)
+            {
+                WriteError(e.ErrorRecord);
+            }
+        }
+        #endregion
+    }
+}
