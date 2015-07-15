06e03d44c177c14cb9533c940a4738ab42015404
Cristopher Robles Ríos <crisrc012@hotmail.com>
2015-07-15 17:05:03 -0600

    Nuevo módulo

---
+++ Modules/AutoLogin/AutoLoginCommon.cs
@@ -0,0 +1,11 @@
+﻿
+namespace AutoLogin
+{
+    public class AutoLoginCommon
+    {
+        public void SetAutoLogin(string key, string name, string value)
+        {
+            Microsoft.Win32.Registry.SetValue(key, name, value, Microsoft.Win32.RegistryValueKind.String);
+        }
+    }
+}
---
+++ Modules/AutoLogin/Set_AutoLogin.cs
@@ -0,0 +1,52 @@
+﻿using System.Management.Automation; //Windows PowerShell NameSpace
+
+namespace AutoLogin
+{
+    [Cmdlet(VerbsCommon.Set, "AutoLogin")]
+    public class Set_AutoLogin : Cmdlet
+    {
+        #region Objects
+        private AutoLoginCommon SAL;
+        #endregion
+        #region Parameters
+        [Parameter(Position = 0, Mandatory = false, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Nombre de la nueva propiedad del registro.")]
+        [ValidateNotNullOrEmpty]
+        public string Name { get; set; }
+        [Parameter(Position = 0, Mandatory = false, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Valor boleano de la propiedad.")]
+        public SwitchParameter Disable
+        {
+            get { return disable; }
+            set { disable = value; }
+        }
+        private bool disable;
+        [Parameter(Position = 0, Mandatory = false, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Valor de la propiedad, en este caso ruta al ejecutable.")]
+        public string Password { get; set; }
+        #endregion
+        #region Methods
+        protected override void BeginProcessing()
+        {
+            SAL = new AutoLoginCommon();
+        }
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                if (disable)
+                {
+                    SAL.SetAutoLogin(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", "AutoAdminLogon", "0");
+                }
+                else
+                {
+                    SAL.SetAutoLogin(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", "AutoAdminLogon", "1");
+                    SAL.SetAutoLogin(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", "DefaultUsername", Name);
+                    SAL.SetAutoLogin(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", "DefaultPassword", Password);
+                }
+            }
+            catch (PSInvalidOperationException e)
+            {
+                WriteError(e.ErrorRecord);
+            }
+        }
+        #endregion
+    }
+}
