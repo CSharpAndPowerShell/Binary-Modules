881e7e3bcacd427a13ba5a58d27b4630184d9cbc
Cristopher Robles Ríos <crisrc012@hotmail.com>
2015-07-15 15:47:01 -0600

    Nuevo módulo

---
+++ Modules/UAC/Set_UAC.cs
@@ -0,0 +1,60 @@
+﻿using System.Management.Automation; //Windows PowerShell NameSpace
+
+namespace UAC
+{
+    [Cmdlet(VerbsCommon.Set, "UAC")]
+    public class Set_UAC : Cmdlet
+    {
+        #region Objects
+        private UACCommon SUAC;
+        #endregion
+        #region Parameters
+        [Parameter(Position = 0, Mandatory = false, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Valor boleano de la propiedad.")]
+        [ValidateNotNullOrEmpty]
+        public SwitchParameter Enable
+        {
+            get { return enable; }
+            set { enable = value; }
+        }
+        private bool enable;
+        [Parameter(Position = 0, Mandatory = false, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Valor boleano de la propiedad.")]
+        [ValidateNotNullOrEmpty]
+        public SwitchParameter Disable
+        {
+            get { return disable; }
+            set { disable = value; }
+        }
+        private bool disable;
+        #endregion
+        #region Methods
+        protected override void BeginProcessing()
+        {
+            SUAC = new UACCommon();
+        }
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                if (!(enable && disable))
+                {
+                    int value = 0;
+                    if (enable)
+                    {
+                        value = 1;
+                    }
+                    if (disable)
+                    {
+                        value = 0;
+                    }
+                    SUAC.Set_UAC(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System", "EnableLUA", value);
+                    SUAC.Set_UAC(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System", "ConsentPromptBehaviorAdmin", value);
+                }
+            }
+            catch (PSInvalidOperationException e)
+            {
+                WriteError(e.ErrorRecord);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Modules/UAC/UACCommon.cs
@@ -0,0 +1,11 @@
+﻿
+namespace UAC
+{
+    class UACCommon
+    {
+        public void Set_UAC(string key, string name, int value)
+        {
+            Microsoft.Win32.Registry.SetValue(key, name, value, Microsoft.Win32.RegistryValueKind.String);
+        }
+    }
+}
