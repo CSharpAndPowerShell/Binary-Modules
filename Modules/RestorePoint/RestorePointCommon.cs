0ff70e5ee49a89a6c753783d160056cc0598cddc
Cristopher Robles Ríos <crisrc012@hotmail.com>
2015-08-05 15:11:13 -0600

    Nuevo método para crear punto de restauración

--- Modules/RestorePoint/RestorePointCommon.cs
+++ Modules/RestorePoint/RestorePointCommon.cs
@@ -1,10 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Management;
 
 namespace RestorePoint
 {
     public class RestorePointCommon
     {
+        public void create_restorepoint()
+        {
+            try
+            {
+                ManagementObject classInstance =
+                    new ManagementObject("root\\DEFAULT",
+                    "SystemRestore", null);
+
+                // Obtain in-parameters for the method
+                ManagementBaseObject inParams =
+                    classInstance.GetMethodParameters("CreateRestorePoint");
+
+                // Add the input parameters.
+
+                // Execute the method and obtain the return values.
+                ManagementBaseObject outParams =
+                    classInstance.InvokeMethod("CreateRestorePoint", inParams, null);
+
+                // List outParams
+                Console.WriteLine("Out parameters:");
+                Console.WriteLine("ReturnValue: " + outParams["ReturnValue"]);
+            }
+            catch (ManagementException err)
+            {
+                MessageBox.Show("An error occurred while trying to execute the WMI method: " + err.Message);
+            }
+        }
     }
 }
