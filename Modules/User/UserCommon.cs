62dd6b264809ea632bf9d2f8ccd6ebd2c9e0b8c7
Cristopher Robles Ríos <crisrc012@hotmail.com>
2015-08-13 14:53:34 -0600

    Licencia

--- Modules/User/UserCommon.cs
+++ Modules/User/UserCommon.cs
@@ -1,4 +1,23 @@
-﻿using System.Collections;
+﻿/*
+CSharpAndPowerShell Modules, tries to help Microsoft Windows admins to write automated scripts easier.
+Copyright(C) 2015  Cristopher Robles Ríos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see<http://www.gnu.org/licenses/>.
+
+*/
+
+using System.Collections;
 using System.DirectoryServices;
 using System.Management;
 
